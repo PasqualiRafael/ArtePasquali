dc2116711023a88a92c4f3078522d816e1fff704
Pasquali <pasqualipiloto@gmail.com>
2023-06-28 05:52:30 -0300

    clients and plains finish

---
+++ dist/js/app.js
@@ -0,0 +1,8 @@
+const brands = document.querySelector("ul.brands");
+total_brand = brands.children.length;
+
+document.documentElement.style.setProperty("$--total-brand", total_brand);
+
+for (let i = 0; i < total_brand; i++) {
+    brands.appendChild(brands.children[i].cloneNode(true));
+}
