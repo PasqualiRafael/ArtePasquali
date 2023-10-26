e5427d4a662e003b24ece0b05ba849403712eabb
Pasquali <pasqualipiloto@gmail.com>
2023-10-26 08:56:56 -0300

    :bug: Fix: img put explict width and height

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const brands = document.querySelector("ul.brands");
-const element = document.querySelector("iframe");
+
 total_brand = brands.children.length;
 
 document.documentElement.style.setProperty("$--total-brand", total_brand);
@@ -7,6 +7,3 @@ document.documentElement.style.setProperty("$--total-brand", total_brand);
 for (let i = 0; i < total_brand; i++) {
     brands.appendChild(brands.children[i].cloneNode(true));
 }
-
-// Add event listener with passive: true option
-element.addEventListener("touchstart", onTouchStart, {passive: true});
