ed0f457dfd8ed05fd5a0ba29b6af38bdd70fbbed
Pasquali <pasqualipiloto@gmail.com>
2023-10-26 08:27:32 -0300

    :bug: Fix: otimization

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 const brands = document.querySelector("ul.brands");
+const element = document.querySelector("iframe");
 total_brand = brands.children.length;
 
 document.documentElement.style.setProperty("$--total-brand", total_brand);
@@ -6,3 +7,6 @@ document.documentElement.style.setProperty("$--total-brand", total_brand);
 for (let i = 0; i < total_brand; i++) {
     brands.appendChild(brands.children[i].cloneNode(true));
 }
+
+// Add event listener with passive: true option
+element.addEventListener("touchstart", onTouchStart, {passive: true});
